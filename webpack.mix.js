126cba79bd1cc8ac117ece4160a3dd0bc05edb05
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-16 20:44:25 +0330

    Updated webpack mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +10,4 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.react('resources/jsx/App.jsx', 'public/jsx/application.js');
