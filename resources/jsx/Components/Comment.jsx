65409c39a27a3dc6a85fceabd01b085bb7c2117a
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-18 19:17:34 +0330

    Added comment

--- resources/jsx/Components/Comment.jsx
+++ resources/jsx/Components/Comment.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Paper, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core'
+
 
 export default function Comment() {
     return (
         <div>
-            comment
+            <Paper>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar src={'/img/user.png'} />
+                    </ListItemAvatar>
+                    <ListItemText primary="Nikan Dalvand" secondary="Oct 12 2019" />
+                </ListItem>
+            </Paper>
         </div>
     )
 }
