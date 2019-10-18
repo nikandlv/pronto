8674979b3dc03cb603a2a7154865a33eb713eadd
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-18 18:55:12 +0330

    Added base of attachment item

--- resources/jsx/Components/Attachments.jsx
+++ resources/jsx/Components/Attachments.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Paper, List } from "@material-ui/core";
+import { Paper, List, ListItem, ListItemText, ListItemAvatar, IconButton } from "@material-ui/core";
+
+import AttachmentIcon from '@material-ui/icons/AttachmentOutlined'
+
+function Attachment(props) {
+    const name = props.name || "filename"
+    return (
+        <ListItem button>
+            <ListItemAvatar>
+                <IconButton>
+                    <AttachmentIcon />
+                </IconButton>
+            </ListItemAvatar>
+            <ListItemText primary={name} />
+        </ListItem>
+    )
+}
 
 export default function Attachments() {
     return (
         <Paper>
             <List>
-                
+                <Attachment />
             </List>
         </Paper>
     )
