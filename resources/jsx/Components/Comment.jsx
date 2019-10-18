b3d15d7ff1b1d4a3abce1988e2c38e44b8925a9c
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-18 19:18:36 +0330

    Added comment style

--- resources/jsx/Components/Comment.jsx
+++ resources/jsx/Components/Comment.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Paper, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
+const useStyles = makeStyles({
+    comment: {
+        borderRadius: 16
+    }
+})
 
 export default function Comment() {
+    const styles = useStyles()
     return (
         <div>
-            <Paper>
+            <Paper className={styles.comment}>
                 <ListItem>
                     <ListItemAvatar>
                         <Avatar src={'/img/user.png'} />
