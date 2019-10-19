ba74600200200f7ed32866ea6dee46f599e0e438
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-19 12:34:55 +0330

    added test for delete test feature

--- tests/Feature/PostsSystemTest.php
+++ tests/Feature/PostsSystemTest.php
@@ -10,6 +10,19 @@ use Tests\TestCase;
 class PostsSystemTest extends TestCase
 {
     use RefreshDatabase;
+//    HELPER FUNCTIONS
+    public function newPost($post = null)
+    {
+        $post = $post!==null ? $post : factory(Post::class)->create();
+
+        $this->postJson('/api/posts' , [
+            'title' => $post->title,
+            'body' => $post->body
+        ]);
+
+        return $post;
+    }
+
     /** @test **/
     public function a_user_can_make_a_new_post_an_get_json_response()
     {
@@ -46,5 +59,20 @@ class PostsSystemTest extends TestCase
 //        then : an validation error will be thrown
     }
 
+    /** @test **/
+    public function a_post_can_be_deleted()
+    {
+        $this->withoutExceptionHandling();
+//        given : we have a post
+        $post = $this->newPost();
+//        when : request is sent to delete the post with method of delete
+        $this->deleteJson('/api/posts/' . $post->id)->assertStatus(200)->assertJson(['message' => 'post deleted successfully']);
+        $this->assertDatabaseMissing('posts', [
+            'id' => $post->id,
+            'title' => $post->title,
+            'body' => $post->body
+        ]);
+//        then : the post must be deleted => it means there must be a message returned with status code and database check
 
+    }
 }
