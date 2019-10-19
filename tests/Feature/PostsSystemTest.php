4b414c3eb4cdb88c302eb084dc1829ef61b5914b
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-19 08:28:19 +0330

    added test for a simple post save feature

---
+++ tests/Feature/PostsSystemTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostsSystemTest extends TestCase
+{
+    /** @test * */
+    public function a_user_can_make_a_new_post()
+    {
+        $this->withoutExceptionHandling();
+//        given  : we have a post
+        $post = factory(Post::class)->create();
+//        when : user tries to save it
+        $this->post('/posts', [
+            'title' => $post->title,
+            'body' => $post->body
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => $post->title,
+            'body' => $post->body
+        ]);
+//        then : there must be a post related to the field in the database
+    }
+}
