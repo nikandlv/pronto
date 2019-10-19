02681d962bcbe897c7d7b9976bb568488c0b3040
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-19 08:44:39 +0330

    finished PostsController to save a post creating request

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function store()
     {
-        dd(\request())
+        Post::create([
+            'title' => \request()->title,
+            'body' => \request()->body
+        ]);
     }
 }
