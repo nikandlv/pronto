77e2d2205bb7932946c030701f3e269cd05d9f7b
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-19 08:31:26 +0330

    modified PostFactory to return a post with content

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->title,
+        'body' => $faker->paragraph
     ];
 });
