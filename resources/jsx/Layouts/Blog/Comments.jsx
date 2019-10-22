8482aadb89fc8560c2f3a1910ff6b87040013654
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-22 08:23:32 +0330

    Created comments topbar

--- resources/jsx/Layouts/Blog/Comments.jsx
+++ resources/jsx/Layouts/Blog/Comments.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import { Grid, Avatar, makeStyles } from '@material-ui/core'
+import { Grid, makeStyles, IconButton } from '@material-ui/core'
 import Comment from '../../Components/Comment'
-import Message from '@material-ui/icons/MessageOutlined'
+import WriteIcon from '@material-ui/icons/EditOutlined'
 import StyledTitle from '../../Components/StyledTitle'
 
 const useStyles = makeStyles({
-    
+    wrapper: {
+        display: 'flex'
+    },
+    push: {
+        flexGrow: 1
+    }
 })
 
 export default function Comments() {
     const styles = useStyles()
     return (
         <section>
-            <div>
+            <div className={styles.wrapper}>
                 <StyledTitle>
                     Comments
                 </StyledTitle>
+                <div className={styles.push}/>
+                <div>
+                <IconButton>
+                    <WriteIcon />
+                </IconButton>
+                </div>
             </div>
             <Grid container spacing={1}>
                 <Grid item xs={12}>
