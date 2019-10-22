4b7078bbba4f309af10697e229dbe4645fcc2c59
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-22 08:17:16 +0330

    Added message icon for comments

--- resources/jsx/Layouts/Blog/Comments.jsx
+++ resources/jsx/Layouts/Blog/Comments.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Avatar } from '@material-ui/core'
 import Comment from '../../Components/Comment'
+import Message from '@material-ui/icons/MessageOutlined'
 
 export default function Comments() {
     return (
         <section>
+            <Avatar>
+                <Message />
+            </Avatar>
             <Grid container spacing={1}>
                 <Grid item xs={12}>
                     <Comment />
