86428bf9bf3fa650cc32f1ecf78015c1264c4a15
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-22 08:21:06 +0330

    ADded comments title

--- resources/jsx/Layouts/Blog/Comments.jsx
+++ resources/jsx/Layouts/Blog/Comments.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Grid, Avatar } from '@material-ui/core'
+import { Grid, Avatar, makeStyles } from '@material-ui/core'
 import Comment from '../../Components/Comment'
 import Message from '@material-ui/icons/MessageOutlined'
+import StyledTitle from '../../Components/StyledTitle'
+
+const useStyles = makeStyles({
+    
+})
 
 export default function Comments() {
+    const styles = useStyles()
     return (
         <section>
-            <Avatar>
-                <Message />
-            </Avatar>
+            <div>
+                <StyledTitle>
+                    Comments
+                </StyledTitle>
+            </div>
             <Grid container spacing={1}>
                 <Grid item xs={12}>
                     <Comment />
