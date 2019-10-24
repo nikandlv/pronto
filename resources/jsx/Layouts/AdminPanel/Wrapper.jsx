c49321f0b6c63cdfe1b36e758cb989e298dbfb81
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-24 06:53:03 +0330

    Removed redundent second part of admin panel menu

--- resources/jsx/Layouts/AdminPanel/Wrapper.jsx
+++ resources/jsx/Layouts/AdminPanel/Wrapper.jsx
@@ -71,15 +71,6 @@ function ResponsiveDrawer(props) {
             <ListItemText primary="test" />
           </ListItem>
       </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
     </div>
   );
 
