0eb9b16a5ccc782cadc52cd665c1c9404bc00bd5
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-24 06:57:19 +0330

    Added posts menu item

--- resources/jsx/Layouts/AdminPanel/Wrapper.jsx
+++ resources/jsx/Layouts/AdminPanel/Wrapper.jsx
@@ -9,6 +9,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ExploreIcon from '@material-ui/icons/ExploreOutlined';
 import CategoryIcon from '@material-ui/icons/CategoryOutlined';
 import SettingsIcon from '@material-ui/icons/SettingsOutlined';
+import PostIcon from '@material-ui/icons/ReceiptOutlined';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Topbar from '../Blog/Topbar';
 import { Box } from '@material-ui/core';
@@ -74,6 +75,12 @@ function ResponsiveDrawer(props) {
               <CategoryIcon />
             </ListItemIcon>
             <ListItemText primary="Categories" />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon>
+              <PostIcon />
+            </ListItemIcon>
+            <ListItemText primary="Posts" />
           </ListItem>
           <ListItem button >
             <ListItemIcon>
