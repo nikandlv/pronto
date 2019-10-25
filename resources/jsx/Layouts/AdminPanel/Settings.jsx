be71098e6caccb924645082af91050a5c6a43ec2
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-25 07:15:39 +0330

    Styled the settings tabs

--- resources/jsx/Layouts/AdminPanel/Settings.jsx
+++ resources/jsx/Layouts/AdminPanel/Settings.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { Tabs, Tab, AppBar } from '@material-ui/core'
+import { Tabs, Tab, AppBar, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    appbar: {
+        borderRadius: '1rem',
+        overflow: 'hidden'
+    }
+})
+
 export default function Settings() {
+    const styles = useStyles()
     return (
         <div>
-            <AppBar position="static" color="default">
+            <AppBar className={styles.appbar} position="static" color="default">
                 <Tabs value={0}  aria-label="simple tabs example">
                     <Tab label="Item One" />
                     <Tab label="Item Two" />
