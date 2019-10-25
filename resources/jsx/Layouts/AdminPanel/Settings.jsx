89b0c5058f873ba2771de06a5f180e3b864d6c89
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-25 07:14:10 +0330

    Added settings tabs

--- resources/jsx/Layouts/AdminPanel/Settings.jsx
+++ resources/jsx/Layouts/AdminPanel/Settings.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Tabs, Tab, AppBar } from '@material-ui/core'
 export default function Settings() {
     return (
         <div>
-            test
+            <AppBar position="static" color="default">
+                <Tabs value={0}  aria-label="simple tabs example">
+                    <Tab label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                </Tabs>
+            </AppBar>
         </div>
     )
 }
