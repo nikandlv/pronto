0c42034eca2f299d0a12807ebac7c8efebd4cbbd
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-25 12:30:33 +0330

    made login function for AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use http\Client;
+use http\Env\Request;
+use http\Message\Body;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+    }
+}
