f97543117af15115fe49fc3464955c2d18c80cd3
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-29 03:30:18 +0330

    Updated settings tabs aria label

--- resources/jsx/Layouts/AdminPanel/Settings.jsx
+++ resources/jsx/Layouts/AdminPanel/Settings.jsx
@@ -24,7 +24,7 @@ export default function Settings() {
                     onChange={(_, tab) => {
                         setTab(tab);
                     }}
-                    aria-label="simple tabs example"
+                    aria-label="Settings tabs"
                 >
                     <Tab label="General" />
                     <Tab label="Posts" />
