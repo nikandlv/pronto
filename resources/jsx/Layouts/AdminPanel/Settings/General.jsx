03da22034ae9aeb14fac0c2b301f9650f3684b74
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-29 03:55:35 +0330

    Fixed the settings grid container

--- resources/jsx/Layouts/AdminPanel/Settings/General.jsx
+++ resources/jsx/Layouts/AdminPanel/Settings/General.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import StyledTitle from "../../../Components/StyledTitle";
 import StyledTextField from "../../../Components/StyledTextField";
 import { Paper, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    container: {
+        width: "100%",
+        margin: "unset"
+    }
+});
 
 export default function General() {
+    const styles = useStyles();
     return (
         <div>
             <StyledTitle>General</StyledTitle>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} className={styles.container}>
                 <Grid xs={12} sm={6} md={4} item>
-                    <StyledTextField fullWidth>Test</StyledTextField>
+                    <StyledTextField fullWidth>Blog title</StyledTextField>
                 </Grid>
                 <Grid xs={12} sm={6} md={4} item>
                     <StyledTextField fullWidth>Test</StyledTextField>
