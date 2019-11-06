f9515b180122c3144da0fa492dfb47b7ac6f3c0b
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-06 03:48:34 +0330

    Cleaned widgets

--- resources/jsx/Layouts/AdminPanel/Widgets.jsx
+++ resources/jsx/Layouts/AdminPanel/Widgets.jsx
@@ -1,25 +1,5 @@
 import React from "react";
-import { Tabs, Tab, AppBar, makeStyles } from "@material-ui/core";
-import SwipeableViews from "react-swipeable-views";
 
-export default function Settings() {
-    const [tab, setTab] = React.useState(0);
-    return (
-        <div>
-            <Tabs
-                value={tab}
-                onChange={(_, tab) => {
-                    setTab(tab);
-                }}
-                aria-label="Settings tabs"
-            >
-                <Tab label="Menu" />
-                <Tab label="Github activity" />
-            </Tabs>
-            <SwipeableViews index={tab}>
-                <div>Menu</div>
-                <div>Github Widget</div>
-            </SwipeableViews>
-        </div>
-    );
+export default function Widgets() {
+    return <div></div>;
 }
