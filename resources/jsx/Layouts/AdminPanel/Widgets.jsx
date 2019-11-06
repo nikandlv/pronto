723f5a7833fd1b4c591dc5ada58881ce8dc84667
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-06 03:36:13 +0330

    added base for widget page

--- resources/jsx/Layouts/AdminPanel/Widgets.jsx
+++ resources/jsx/Layouts/AdminPanel/Widgets.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { Tabs, Tab, AppBar, makeStyles } from "@material-ui/core";
+import SwipeableViews from "react-swipeable-views";
 
-export default function Widgets() {
-    return <div>widgets</div>;
+const useStyles = makeStyles({
+    appbar: {
+        borderRadius: "1rem",
+        overflow: "hidden"
+    }
+});
+
+export default function Settings() {
+    const styles = useStyles();
+    const [tab, setTab] = React.useState(0);
+    return (
+        <div>
+            <Tabs
+                value={tab}
+                onChange={(_, tab) => {
+                    setTab(tab);
+                }}
+                aria-label="Settings tabs"
+            >
+                <Tab label="Menu" />
+                <Tab label="Github activity" />
+            </Tabs>
+            <SwipeableViews index={tab}>
+                <div>Menu</div>
+                <div>Github Widget</div>
+            </SwipeableViews>
+        </div>
+    );
 }
