fe59898f23aa741b1b5e67e0e79416bcba34435c
noisyBoy-9 <sina.shariati@yahoo.com>
2019-11-09 18:07:03 +0330

    added test for path method in User model

--- tests/Unit/UserUnitTest.php
+++ tests/Unit/UserUnitTest.php
@@ -2,10 +2,21 @@
 
 namespace Tests\Unit;
 
+
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserUnitTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /** @test **/
+    public function it_knows_its_path()
+    {
+        $user = factory(User::class)->create();
+
+        $this->assertEquals('/users/' . $user->id, $user->path());
+    }
 }
