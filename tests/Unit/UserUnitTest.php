3f983de9bbca9884e67a6706ac6f8be976f82ed8
noisyBoy-9 <sina.shariati@yahoo.com>
2019-11-09 18:04:24 +0330

    created user unit test

---
+++ tests/Unit/UserUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserUnitTest extends TestCase
+{
+}
