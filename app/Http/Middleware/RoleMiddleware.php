eafbc0470e20d156518f95f82ded796190bc57c7
noisyBoy-9 <sina.shariati@yahoo.com>
2019-11-12 00:37:51 +0330

    finished role middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\pronto\users\UserRoleManager;
 use Closure;
 
 class RoleMiddleware
@@ -9,13 +10,28 @@ class RoleMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next, $role)
     {
-        dd($role);
-        return $next($request);
+
+//        role is a string changing it to role numbers defined in the RoleManger File
+        switch ($role) {
+            case 'admin' :
+                $role = UserRoleManager::ROLE_ADMIN;
+                break;
+            case 'member' :
+                $role = UserRoleManager::ROLE_MEMBER;
+                break;
+            case 'author' :
+                $role = UserRoleManager::ROLE_AUTHOR;
+                break;
+        }
+
+        if (auth()->user()->role === $role) {
+            return $next($request);
+        }
     }
 }
