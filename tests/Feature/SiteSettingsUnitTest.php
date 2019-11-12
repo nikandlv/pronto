ae47eb3750ac3be870149be122a89895d9ecb740
noisyBoy-9 <sina.shariati@yahoo.com>
2019-11-13 00:12:02 +0330

    finished test for alloweds property in site settings

--- tests/Feature/SiteSettingsUnitTest.php
+++ tests/Feature/SiteSettingsUnitTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\SiteSetting;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,5 +11,13 @@ class SiteSettingsUnitTest extends TestCase
 {
     use  RefreshDatabase;
 
+    /** @test **/
+    public function it_has_a_set_of_values_as_allowed_values_to_store_as_setting_key_in_database()
+    {
+        $this->withoutExceptionHandling();
 
+        $siteSetting = new  SiteSetting();
+
+        $this->assertIsArray($siteSetting->alloweds);
+    }
 }
