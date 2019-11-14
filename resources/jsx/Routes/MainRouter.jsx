119cb2fec78b2bd14849ea5c9b987490ac4ab078
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-14 22:34:18 +0330

    Cleaned up some codes

--- resources/jsx/Routes/MainRouter.jsx
+++ resources/jsx/Routes/MainRouter.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import {Switch, Route, BrowserRouter} from 'react-router-dom'
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
-import Wrapper from '../Layouts/Wrapper'
-import BlogRouter from './BlogRouter'
-import AdminRouter from './AdminRouter'
-import PanelRouter from './PanelRouter'
+import Wrapper from "../Layouts/Wrapper";
+import BlogRouter from "./BlogRouter";
+import PanelRouter from "./PanelRouter";
 
 export default function MainRouter(props) {
     return (
-            <BrowserRouter>
-                <Wrapper>
-                        <Switch>
-                            <Route path="/panel/*" component={PanelRouter} {...props}/>
-                            <Route path="/admin/*" component={AdminRouter} {...props}/>
-                            <Route path="/admin" component={AdminRouter} exact />
-                            <Route path="/*" component={BlogRouter} {...props}/>
-                        </Switch>
-                </Wrapper>
-            </BrowserRouter>
-    )
+        <BrowserRouter>
+            <Wrapper>
+                <Switch>
+                    <Route path="/admin/*" component={PanelRouter} {...props} />
+                    <Route
+                        path="/admin/"
+                        component={PanelRouter}
+                        {...props}
+                        exact
+                    />
+                    <Route path="/*" component={BlogRouter} {...props} />
+                </Switch>
+            </Wrapper>
+        </BrowserRouter>
+    );
 }
