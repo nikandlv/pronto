e8f5ec56d788249ada06455e5342344cb6a79229
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-14 05:11:04 +0330

    Added tje avatar

--- resources/jsx/Components/StatsCard.jsx
+++ resources/jsx/Components/StatsCard.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Card, CardContent, ListItem, ListItemText } from "@material-ui/core";
+import {
+    Card,
+    CardContent,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    Avatar
+} from "@material-ui/core";
 
-export default function() {
+import Person from "@material-ui/icons/PersonOutlineOutlined";
+
+export default function StatsCard() {
     return (
         <Card>
             <CardContent>
                 <ListItem>
                     <ListItemText primary="Test" secondary="34K" />
+                    <ListItemAvatar>
+                        <Avatar>
+                            <Person />
+                        </Avatar>
+                    </ListItemAvatar>
                 </ListItem>
             </CardContent>
         </Card>
