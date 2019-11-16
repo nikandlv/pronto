58c4c5d2cc48741598665a7c53b1e2e02e4fbdec
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-17 01:26:08 +0330

    Added edit category dialog actions

--- resources/jsx/Data/Actions/CategoryDialogActions.jsx
+++ resources/jsx/Data/Actions/CategoryDialogActions.jsx
@@ -1,4 +1,4 @@
-import { NEW_CATEGORY_DIALOG } from "./types";
+import { NEW_CATEGORY_DIALOG, EDIT_CATEGORY_DIALOG } from "./types";
 
 export const openNewCategoryDialog = () => dispatch => {
     dispatch({
@@ -9,5 +9,17 @@ export const openNewCategoryDialog = () => dispatch => {
 export const closeNewCategoryDialog = () => dispatch => {
     dispatch({
         type: NEW_CATEGORY_DIALOG.CLOSE_NEW_CATEGORY_DIALOG
+    });
+};
+
+export const openEditCategoryDialog = () => dispatch => {
+    dispatch({
+        type: EDIT_CATEGORY_DIALOG.OPEN_EDIT_CATEGORY_DIALOG
+    });
+};
+
+export const closeEditCategoryDialog = () => dispatch => {
+    dispatch({
+        type: EDIT_CATEGORY_DIALOG.CLOSE_EDIT_CATEGORY_DIALOG
     });
 };
