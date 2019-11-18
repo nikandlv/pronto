f8bdabaf650779924cf6de74d75ac11929d9ca1c
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-18 04:44:43 +0330

    Added new post page layout

--- resources/jsx/Layouts/Panel/NewPost.jsx
+++ resources/jsx/Layouts/Panel/NewPost.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Box, Grid, Paper } from "@material-ui/core";
 
 export default function NewPost() {
-    return (<div>
-        New Post
-    </div>)
+    return (
+        <Box m={2}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} md={4}>
+                    <Paper>a</Paper>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Paper>a</Paper>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
