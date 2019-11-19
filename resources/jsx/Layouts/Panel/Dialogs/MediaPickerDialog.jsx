aac7bfbead866d3a1637b74f513de72d35c1bc47
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-19 07:24:13 +0330

    Added base layout of media picker

--- resources/jsx/Layouts/Panel/Dialogs/MediaPickerDialog.jsx
+++ resources/jsx/Layouts/Panel/Dialogs/MediaPickerDialog.jsx
@@ -1,15 +1,90 @@
-import React from 'react'
-import withDynamic from '../../../Data/withDynamic';
-import {Dialog, DialogContent, Grid, Drawer} from '@material-ui/core'
+import React from "react";
+import withDynamic from "../../../Data/withDynamic";
+import {
+    Dialog,
+    Grid,
+    Paper,
+    DialogContent,
+    makeStyles
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    sidebar: {
+        display: "flex",
+        flexDirection: "row",
+        minHeight: "calc(100vh - 96px)",
+        maxHeight: "calc(100vh - 96px)"
+    },
+    content: {
+        height: "100%",
+        minHeight: "calc(100vh - 96px)",
+        maxHeight: "calc(100vh - 96px)"
+    }
+});
+
 function MediaPickerDialog(props) {
     const state = props.MediaPicker;
-    return <Dialog open={state.open} fullWidth maxWidth="lg">
-        <DialogContent>
-            <Drawer variant="permanent">
-                abc
-            </Drawer>
-        </DialogContent>
-    </Dialog>
+    const styles = useStyles();
+    return (
+        <Dialog open={state.open} fullWidth maxWidth="lg">
+            <Grid container>
+                <Grid item xs={12} sm={3}>
+                    <Paper className={styles.sidebar}>abc</Paper>
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                    <DialogContent className={styles.content}>
+                        abc
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        ab abc
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        ab abc
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        ab abc
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        ab
+                    </DialogContent>
+                </Grid>
+            </Grid>
+        </Dialog>
+    );
 }
 
-export default withDynamic(MediaPickerDialog).injectReducer("MediaPicker").build()
+export default withDynamic(MediaPickerDialog)
+    .injectReducer("MediaPicker")
+    .build();
