9f03f4f57833dcb7d9612ec0714309f8a97ca967
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-20 08:54:07 +0330

    Added add icon and divider

--- resources/jsx/Layouts/Panel/NewPost.jsx
+++ resources/jsx/Layouts/Panel/NewPost.jsx
@@ -245,7 +245,9 @@ export default function NewPost() {
                     <Card className={styles.card}>
                         <Tabs>
                             <Tab label="English" />
+                            <Tab icon={<AddIcon />} />
                         </Tabs>
+                        <Divider variant="middle" />
                         <CardContent>
                             <Grid container spacing="3">
                                 <Grid item xs={12} md={6}>
