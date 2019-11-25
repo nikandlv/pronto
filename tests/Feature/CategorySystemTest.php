36a764e6caab3c85fe01814f5f13e6dc35092e2c
noisyBoy-9 <sina.shariati@yahoo.com>
2019-11-25 23:33:51 +0330

    made test file for category feature

---
+++ tests/Feature/CategorySystemTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategorySystemTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test **/
+    public function () 
+    {
+        
+    }
+}
