6cfebe2dcbc10099b3f1ee35631317dae707e38a
noisyBoy-9 <sina.shariati@yahoo.com>
2020-01-02 08:52:15 +0330

    made test for media upload test

---
+++ app/pronto/storage/FileUploadTypeMangement.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\pronto\storage;
+
+
+class FileUploadTypeMangement
+{
+    const TYPE_MEDIA = 1;
+    const TYPE_ATTACHEMENT = 2;
+    const TYPE_UPLOADS = 3;
+}
---
+++ tests/Feature/StorageSystemTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\pronto\storage\FileUploadTypeMangement;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class StorageSystemTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test **/
+    public function a_authenticated_user_can_upload_a_media()
+    {
+        $this->withoutExceptionHandling();
+        $user = $this->signIn();
+
+        Storage::fake('files');
+
+        $file = UploadedFile::fake()->image('media.jpg');
+
+        $this->postJson('/api/files/media', [
+            'file' => $file,
+            'name' => $file->name,
+        ]);
+
+        $this->assertDatabaseHas('files', [
+            'name' => $file->hashName(),
+            'path' => 'storage/files/' . Carbon::today() . '/' . $file->hashName(),
+            'type' => FileUploadTypeMangement::TYPE_MEDIA,
+            'owner_id' => $user->id
+        ]);
+    }
+}
