bb1842c4b9745acead4ed3ade6110c561b63a72f
noisyBoy-9 <sina.shariati@yahoo.com>
2020-01-02 10:03:13 +0330

    controller for simple file upload

--- app/Http/Controllers/UploadMediaController.php
+++ app/Http/Controllers/UploadMediaController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers;
 
 
 
+use App\File;
+use App\pronto\storage\FileUploadTypeMangement;
+use Carbon\Carbon;
+
 class UploadMediaController extends Controller
 {
     public function store()
     {
         $file = request()->file();
 
+        auth()->user()->files()->create([
+            'name' => $file->hashName(),
+            'path' => '/storage/files/media' . Carbon::today() . '/' . $file->hashName(),
+            'type' => FileUploadTypeMangement::TYPE_MEDIA,
+        ]);
     }
-
 }
